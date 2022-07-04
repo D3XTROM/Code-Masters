226b724c662e80548af7e6b552701da2d005765e
D3XTROM <108614826+D3XTROM@users.noreply.github.com>
2022-07-04 16:38:46 +0300

    Задача 1433 (Типы данных)

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести количество клеток и кроликов: ");
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            if (n < m)
+                Console.WriteLine(m + 1 - n);
+            else
+                Console.WriteLine("1");
+        }
+    }
+}
