bc20b8ed391fbcb864866b4333e39849df1bdaee
D3XTROM <108614826+D3XTROM@users.noreply.github.com>
2022-07-04 12:36:36 +0300

    Задача 63

---
+++ Ex 2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число ячеек");
+int N = int.Parse(Console.ReadLine());
+
+
+var A = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+for (int i = 0; i < N; i++)
+{
+    if (A[i] % 2 == 0)
+    {
+        Console.Write($"{i}");
+    }
+
+}
