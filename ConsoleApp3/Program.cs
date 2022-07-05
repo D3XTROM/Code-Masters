18604cb78f587bbb7db9ce9b25519a2b54b5b23d
D3XTROM <108614826+D3XTROM@users.noreply.github.com>
2022-07-05 20:38:07 +0300

    Задача №306. Минимум 4 чисел

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Написать 4 числа: ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int d = int.Parse(Console.ReadLine());
+if (a <= b && a <= c && a <= d)
+    Console.WriteLine(a);
+else if (b <= c && b <= d && b <= a)
+    Console.WriteLine(b);
+else if (c <= d && c <= a && c <= b)
+    Console.WriteLine(c);
+else Console.WriteLine(d);
+    
