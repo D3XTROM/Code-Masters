7c455d24aa4bed019654e1cd78326c5e58b36b57
D3XTROM <108614826+D3XTROM@users.noreply.github.com>
2022-07-10 17:12:07 +0300

    Задача №65. Количество положительных элементов

---
+++ Массивы.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Ввести количество элементов массива:");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Ввести элемент с индексом {i}: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int m = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+                if (array[i] >= 0)
+                    m++;
+            }
+            Console.WriteLine(m);
+        }
+    }
+}
