8f5fd0a053010d4212f17a655e0b191b52d9d5c6
D3XTROM <108614826+D3XTROM@users.noreply.github.com>
2022-07-11 15:00:14 +0300

    Add files via upload

---
+++ функции.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static int Minimum(int a, int b, int c, int d)
+        {
+
+           
+
+            if (a <= b && a <= c && a <= d)
+                return a;
+            else if (b <= a && b <= c && b <= d)
+                return b;
+            else if (c <= a && c <= d && c <= b)
+                return c;
+            else return d;
+
+        }
+        static void Main()
+        {
+            Console.WriteLine("Ввести 4 числа:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int min;
+            min = Minimum(a, b, c, d);
+            Console.WriteLine(min);
+        }
+
+    }
+}
