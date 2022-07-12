74d91cc622160b94e1deb4941c79ff406fcd6e38
D3XTROM <108614826+D3XTROM@users.noreply.github.com>
2022-07-12 18:01:01 +0300

    Функции Задача №307. Степень
    Входные данные
    Вводится 2 числа - a (вещественное) и n (целое неотрицательное).
    
    Выходные данные
    Необходимо вывести  значение an.

---
+++ Задача №307. Степень.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+       
+        static int Main(string[] args)
+        {
+            Console.Write($"Ввести 2 числа, 1-е число, 2-е его степень:\t");
+            double a = double.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (b < 0)
+            {
+                Console.WriteLine("b не может быть отрицательным");
+                b = int.Parse(Console.ReadLine());
+
+            }
+            double c = Math.Pow(a, b);
+            Console.WriteLine(c);
+            return (int)c;
+            
+        }
+    }
+}
