d6426ba3d0fc1083901dff02f1b7e2a26109a884
D3XTROM <108614826+D3XTROM@users.noreply.github.com>
2022-07-17 11:37:29 +0300

    Задача №341. Количество делителей

---
+++ Делители.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Deliteli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            int count = 0;
+            int val;
+            if (Int32.TryParse(Console.ReadLine(), out val))
+            {
+                
+                for (int i = 1; i <= val; i++)
+                    if (val % i == 0)
+                    {
+                        
+                        count++;
+                    }
+                Console.WriteLine($"Количество делителей числа = {count}");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Ещё раз");
+                Console.ReadLine();
+            }
+
+            
+        }
+    }
+}
