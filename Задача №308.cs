89a6e63b36f15125faac94c766a8f520ceb4ebeb
D3XTROM <108614826+D3XTROM@users.noreply.github.com>
2022-07-20 11:46:13 +0300

    Задача №308. Исключающее ИЛИ

---
+++ Задача №308.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+
+        static int Main()
+        {
+            Console.WriteLine("Ввести x и y, они должны быть ровны 1 или 0: ");
+            int x; int y;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            if (x == 0 && y == 0)
+                Console.WriteLine("false");
+            else if (x == 1 && y == 1)
+                Console.WriteLine("false");
+            else if (x == 1 && y == 0) 
+                Console.WriteLine("true");
+            else Console.WriteLine("true");
+            return x;
+        }
+    }
+}
