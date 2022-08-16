00c01e68be6e035cc3731fbef02c125b1774ec4d
D3XTROM <108614826+D3XTROM@users.noreply.github.com>
2022-08-16 21:44:03 +0300

    Project SQL

---
+++ WindowsFormsApp1/WindowsFormsApp1/Login.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LogInBUTTON = new System.Windows.Forms.Button();
+            this.PassText = new System.Windows.Forms.TextBox();
+            this.LoginText = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Esc = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Controls.Add(this.LogInBUTTON);
+            this.panel1.Controls.Add(this.PassText);
+            this.panel1.Controls.Add(this.LoginText);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(370, 490);
+            this.panel1.TabIndex = 0;
+            // 
+            // LogInBUTTON
+            // 
+            this.LogInBUTTON.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LogInBUTTON.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LogInBUTTON.Location = new System.Drawing.Point(106, 404);
+            this.LogInBUTTON.Name = "LogInBUTTON";
+            this.LogInBUTTON.Size = new System.Drawing.Size(170, 65);
+            this.LogInBUTTON.TabIndex = 6;
+            this.LogInBUTTON.Text = "Вход";
+            this.LogInBUTTON.UseVisualStyleBackColor = true;
+            // 
+            // PassText
+            // 
+            this.PassText.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PassText.Location = new System.Drawing.Point(117, 308);
+            this.PassText.Name = "PassText";
+            this.PassText.Size = new System.Drawing.Size(241, 27);
+            this.PassText.TabIndex = 5;
+            this.PassText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.PassText.UseSystemPasswordChar = true;
+            this.PassText.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // LoginText
+            // 
+            this.LoginText.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoginText.Location = new System.Drawing.Point(117, 183);
+            this.LoginText.Name = "LoginText";
+            this.LoginText.Size = new System.Drawing.Size(239, 27);
+            this.LoginText.TabIndex = 4;
+            this.LoginText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.LoginText.TextChanged += new System.EventHandler(this.LoginText_TextChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(27, 295);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(61, 49);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(27, 173);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(61, 49);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel2.Controls.Add(this.Esc);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(370, 100);
+            this.panel2.TabIndex = 0;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel2_MouseDown);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Panel2_MouseMove);
+            // 
+            // Esc
+            // 
+            this.Esc.AutoSize = true;
+            this.Esc.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Esc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Esc.ForeColor = System.Drawing.Color.Snow;
+            this.Esc.Location = new System.Drawing.Point(349, 0);
+            this.Esc.Name = "Esc";
+            this.Esc.Size = new System.Drawing.Size(21, 20);
+            this.Esc.TabIndex = 1;
+            this.Esc.Text = "X";
+            this.Esc.Click += new System.EventHandler(this.Esc_Click);
+            this.Esc.MouseEnter += new System.EventHandler(this.Esc_MouseEnter);
+            this.Esc.MouseLeave += new System.EventHandler(this.Esc_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(100, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Авторизация ";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 490);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.Text = "Login";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox PassText;
+        private System.Windows.Forms.TextBox LoginText;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button LogInBUTTON;
+        private System.Windows.Forms.Label Esc;
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Login.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Esc_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Esc_MouseEnter(object sender, EventArgs e)
+        {
+            Esc.ForeColor = Color.Red;
+            
+        }
+
+        private void Esc_MouseLeave(object sender, EventArgs e)
+        {
+            Esc.ForeColor = Color.White;
+        }
+        
+        Point LastPoint;
+
+        private void Panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - LastPoint.X;
+                this.Top+=e.Y - LastPoint.Y;
+
+            }
+
+        }
+
+        private void Panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            LastPoint = new Point(e.X, e.Y);
+        }
+    }
+}
